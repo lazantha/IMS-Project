54c7df0e41d185df0a71678cdd56c9d895fccd76
lazantha <sglasanthapradeep@gmail.com>
2024-06-08 11:35:15 +0530

    sign segment updated

--- resources/views/templates/signIn.blade.php
+++ resources/views/templates/signIn.blade.php
@@ -2,19 +2,17 @@
 @section('Sign in')
 @section('content')
     
-<div class="container mt-4">
+<div class="container mt-4 ">
     <div class="row justify-content-center">
         <div class="col-lg-6">
-            <form class="card p-4 shadow-sm border-warning" action="{{route('signIn')}}" method="POST">
+            <form class="card p-4 shadow " action="{{route('signIn')}}" method="POST">
                 @csrf
                 <h2 class="text-center mb-4">Login</h2>
                 <div class="mb-3">
-                    <label for="exampleInputEmail1" class="form-label">Email address</label>
-                    <input type="email" name="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
+                    <input type="email" name="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Email">
                     <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
                 </div>
                 <div class="mb-3">
-                    <label for="exampleInputPassword1" class="form-label">Password</label>
                     <input type="password" name="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
                 </div>
                 <div class="mb-3 form-check">
--- resources/views/templates/signUp.blade.php
+++ resources/views/templates/signUp.blade.php
@@ -5,33 +5,28 @@
 <div class="container mt-4">
     <div class="row justify-content-center">
         <div class="col-lg-6">
-            <form class="card p-4 shadow-sm border-warning" action="{{route('signUp')}}" method="POST">
+            <form class="card p-4 shadow" action="{{route('signUp')}}" method="POST">
                 @csrf
                 <h2 class="text-center mb-4">Sign Up</h2>
                 <div class="mb-3">
-                    <label for="exampleInputEmail1" class="form-label">First Name</label>
                     <input type="text" name="firstName" class="form-control" id="firstName"  placeholder="First Name">
                 </div>
 
                 <div class="mb-3">
-                    <label for="exampleInputEmail1" class="form-label">Last Name</label>
                     <input type="text" name="lastName" class="form-control" id="lastName"  placeholder="Last Name">
                 </div>
 
                 
                 <div class="mb-3">
-                    <label for="exampleInputEmail1" class="form-label">Email address</label>
                     <input type="email" name="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
                     <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
                 </div>
 
                 <div class="mb-3">
-                    <label for="exampleInputPassword1" class="form-label">Password</label>
                     <input type="password" name="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
                 </div>
 
                 <div class="mb-3">
-                    <label for="exampleInputPassword1" class="form-label">Re Enter Password</label>
                     <input type="password" name="rePassword" class="form-control" id="rePassword" placeholder="Re Enter Password">
                 </div>
                 
